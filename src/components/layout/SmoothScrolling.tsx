fcf35f376a02e0a5f6930e7aa35f6e21bc6bb1f6
xeanoo <boutrinambroise@gmail.com>
2026-03-10 18:42:45 +0100

    correction smooth scrool

--- src/components/layout/SmoothScrolling.tsx
+++ src/components/layout/SmoothScrolling.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from 'lenis/react';
 
 export function SmoothScrolling({ children }: { children: React.ReactNode }) {
     return (
-        <ReactLenis root options={{ lerp: 0.08, duration: 1.2, smoothWheel: true }}>
+        <ReactLenis root options={{ lerp: 0.08, duration: 1.2, smoothWheel: true, syncTouch: false }}>
             {children}
         </ReactLenis>
     );
